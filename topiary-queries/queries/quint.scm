b640b6e47441917878b0b56c27559a9f4b96c3f5
bugarela <gabrielamoreira05@gmail.com>
2024-12-24 05:47:28 -0300

    Fix if-then-else

--- topiary-queries/queries/quint.scm
+++ topiary-queries/queries/quint.scm
@@ -81,10 +81,18 @@
 )
 
 (
-  "(" @append_empty_softline @append_indent_start @append_antispace
+ "if"? @do_nothing
+  "(" @append_empty_softline @append_indent_start
   _
-  ")" @prepend_empty_softline @prepend_indent_end @prepend_antispace
+  ")" @prepend_empty_softline @prepend_indent_end
 )
+
+(
+  "(" @append_antispace
+  _
+  ")" @prepend_antispace
+)
+
 
 (operator_application
   (qualified_identifier) @append_antispace
@@ -116,16 +124,15 @@
   (expr)
 )
 
-;; TODO: fix me, this is not working and we should probably not ident "else if"s
 (if_else_condition
  (expr) @prepend_antispace @append_antispace
  .
- (expr) @append_spaced_softline @append_indent_start
+ (expr) @append_spaced_softline @prepend_indent_start @append_indent_end @prepend_spaced_softline
  .
- "else" @prepend_indent_end @append_indent_start
  (expr
-   (if_else_condition)* @do_nothing
- ) @append_indent_end
+   ;; don't over-indent "else if" blocks
+   (if_else_condition)* @prepend_indent_end @append_indent_start
+ ) @prepend_indent_start @append_indent_end
 )
 
 ; Never put a space before a comma
